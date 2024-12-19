752708b7dff1b3ac2fad60d0f40822d3e1a76d45
saronzdev <ronaldsardinas05@gmail.com>
2024-12-19 14:02:20 -0500

    new features

--- index.js
+++ index.js
@@ -5,64 +5,24 @@ import connectDB from './src/database/db.connect.js'
 import {login, register} from './src/controllers/auth.controller.js'
 import users from './src/routes/users.route.js'
 import posts from './src/routes/posts.route.js'
-import expressSession from 'express'
+import  session from 'express-session'
 
 const PORT = process.env.PORT || 3000
 const PATH = '/api/v1/'
 const app = express()
 app.disable('x-powered-by')
 
-if (process.env.NODE_ENV === "development"){
-  app.use(
-    cors({
-      origin: "https://localhost:3000",
-      credentials: true,
-    })
-  )
-}
 
-if (process.env.NODE_ENV === "production"){
-  app.use(
-    cors({
-      origin: "https://bird-dusky.vercel.app",
-      credentials: true,
-    })
-  )
-}
-
-if (process.env.NODE_ENV === "development"){
-  res.cookie("token", token, {
-  // can only be accessed by server requests
-  httpOnly: true,
-  // path = where the cookie is valid
-  path: "/",
-  // domain = what domain the cookie is valid on
-   domain: "localhost",
-  // secure = only send cookie over https
-  secure: false,
-  // sameSite = only send cookie if the request is coming from the same origin
-  sameSite: "lax", // "strict" | "lax" | "none" (secure must be true)
-  // maxAge = how long the cookie is valid for in milliseconds
-  maxAge: 3600000, // 1 hour
-})
-}
-
-if (process.env.NODE_ENV === "production"){
-  res.cookie("token", token, {
-  // can only be accessed by server requests
-  httpOnly: true,
-  // path = where the cookie is valid
-  path: "/",
-  // secure = only send cookie over https
-  secure: true,
-  // sameSite = only send cookie if the request is coming from the same origin
-  sameSite: "none", // "strict" | "lax" | "none" (secure must be true)
-  // maxAge = how long the cookie is valid for in milliseconds
-  maxAge: 3600000, // 1 hour
-})
-}
-
-
+app.use(cors())
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: true,
+    sameSite: 'none'
+  }
+}))
 app.use(express.static(path.join(process.cwd(), 'public')), express.json(), express.urlencoded({extended: false}))
 app.use(connectDB)
 app.post(PATH + 'register', register)
