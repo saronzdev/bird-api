c35e59d6aa7c19e6893fab7ef8619e66889b5b2c
saronzdev <ronaldsardinas05@gmail.com>
2024-12-19 13:24:49 -0500

    new features

--- index.js
+++ index.js
@@ -12,14 +12,57 @@ const PATH = '/api/v1/'
 const app = express()
 app.disable('x-powered-by')
 
-app.use(cors())
-app.use(expressSession({
-  // ... other options
-   cookie: {
-     secure: 'auto',
-     sameSite: 'none'
-   }
- }))
+if (process.env.NODE_ENV === "development"){
+  app.use(
+    cors({
+      origin: "https://localhost:3000",
+      credentials: true,
+    })
+  )
+}
+
+if (process.env.NODE_ENV === "production"){
+  app.use(
+    cors({
+      origin: "https://bird-dusky.vercel.app",
+      credentials: true,
+    })
+  )
+}
+
+if (process.env.NODE_ENV === "development"){
+  res.cookie("token", token, {
+  // can only be accessed by server requests
+  httpOnly: true,
+  // path = where the cookie is valid
+  path: "/",
+  // domain = what domain the cookie is valid on
+   domain: "localhost",
+  // secure = only send cookie over https
+  secure: false,
+  // sameSite = only send cookie if the request is coming from the same origin
+  sameSite: "lax", // "strict" | "lax" | "none" (secure must be true)
+  // maxAge = how long the cookie is valid for in milliseconds
+  maxAge: 3600000, // 1 hour
+})
+}
+
+if (process.env.NODE_ENV === "production"){
+  res.cookie("token", token, {
+  // can only be accessed by server requests
+  httpOnly: true,
+  // path = where the cookie is valid
+  path: "/",
+  // secure = only send cookie over https
+  secure: true,
+  // sameSite = only send cookie if the request is coming from the same origin
+  sameSite: "none", // "strict" | "lax" | "none" (secure must be true)
+  // maxAge = how long the cookie is valid for in milliseconds
+  maxAge: 3600000, // 1 hour
+})
+}
+
+
 app.use(express.static(path.join(process.cwd(), 'public')), express.json(), express.urlencoded({extended: false}))
 app.use(connectDB)
 app.post(PATH + 'register', register)
